658c243871c416a8f4ca40e098d14771f690b787
falseDev <64366844+falseAge@users.noreply.github.com>
2024-10-01 20:26:28 +0500

    update unput system, blend-tree, camera and add character controller

--- Assets/Scripts/Player/PlayerMover.cs
+++ Assets/Scripts/Player/PlayerMover.cs
@@ -1,82 +1,181 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro;
-using Unity.VisualScripting;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.TextCore.Text;
 
-public class PlayerMover : MonoBehaviour
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(PlayerInput))]
+
+public class PlayerMovementController : MonoBehaviour
 {
-    [SerializeField] private PlayerInputs _inputs;
-    [SerializeField] private float _speed;
-    [SerializeField] private float _jumpForce;
-    [SerializeField] private float _mouseSense;
 
-    private Rigidbody _rb;
-    private Camera _camera;
-    private Animator _anim;
+    public static PlayerMovementController Current { get; private set; } = null;
 
-    private float _xRotation = 0;
-    private float _yRotation = 0;
+    [SerializeField] private Camera headCamera;
+    [SerializeField] private Animator _animator;
+    [SerializeField, Range(0, 50)] private float speed = 10;
+    [SerializeField, Range(0, 50)] private float sprintSpeed = 10;
+    [SerializeField, Range(0, 2)] private float characterSense = 0.2f;
+
+
+    public bool IsGrounded { get; private set; } = false;
+
+    private CharacterController characterController;
+    private PlayerInput input;
+    
+    private Vector3 movementDirection = Vector3.zero;
+    private Vector2 lookDirection = Vector2.zero;
+
+    private Vector3 localMovementAcelerationVector = Vector3.zero;
+
+    private bool SprintState = false;
+    
+    private Vector3 velocity = Vector3.zero;
+    private float contollerHitResetTimeout = 0;
+
+    private Vector3 resultmovementDirection = Vector3.zero;
+
+    public void StartControlling()
+    {
+        input.enabled = true;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    public void StopControlling()
+    {
+        input.enabled = false;
+
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
 
     private void Awake()
     {
-        _rb = GetComponent<Rigidbody>();
-        _camera = Camera.main;
-        _anim = GetComponent<Animator>();        
-
-        _inputs.attackEvent.AddListener(OnAttack);
-        _inputs.jumpEvent.AddListener(OnJump);
+        characterController = GetComponent<CharacterController>();
+        input = GetComponent<PlayerInput>();
+    
+        StartControlling();
+        Current = this;
     }
-
-    void Update()
+    private void FixedUpdate()
     {
-        OnMove();
-        OnLook();
+        ResetCollisionData();
+        CalculateVelocity(ref velocity);
+
+        resultmovementDirection = velocity * 5f + CalculateMovementDirection();
     }
-
-    private void OnMove()
+    private void LateUpdate()
     {
-        if (_inputs.move.x == 1)
-        {
-            _anim.SetTrigger("runForward");
-        }
-        else if (_inputs.move.x == -1)
-        {
-            _anim.SetTrigger("runBackward");
-        }
-        else if (_inputs.move.y == 1)
-        {
-            _anim.SetTrigger("runRight");
-        }
-        else if (_inputs.move.y == -1)
-        {
-            _anim.SetTrigger("runLeft");
-        }
+        var timescale = Time.deltaTime * 20f;
 
-        _rb.AddRelativeForce(new Vector3(_inputs.move.x, 0, _inputs.move.y) * _speed * Time.deltaTime);
-    }
-
-    private void OnLook()
-    {
-        _xRotation -= _inputs.look.y;
-        _xRotation = Mathf.Clamp(_xRotation, -90f, 90f);
-
-        _yRotation += _inputs.look.x;
-
-        _camera.transform.localRotation = Quaternion.Euler(_xRotation, 0, 0);
-        transform.rotation = Quaternion.Euler(0, _yRotation, 0);
-    }
-
-    private void OnAttack()
-    {
+        transform.rotation = 
+            Quaternion.Lerp(
+                transform.rotation, 
+                Quaternion.Euler(0, lookDirection.x, 0), 
+                timescale);
         
+        headCamera.transform.localRotation = 
+            Quaternion.Lerp(
+                headCamera.transform.localRotation, 
+                Quaternion.Euler(-lookDirection.y, 0, 0), 
+                timescale);
+
+        characterController.Move(resultmovementDirection * Time.deltaTime);
     }
 
-    private void OnJump()
+#if UNITY_EDITOR
+    private void OnValidate()
     {
-        _anim.SetTrigger("jump");
-        _rb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse);
+        if (headCamera == null)
+        {
+            headCamera = GetComponentInChildren<Camera>();
+        }
+    }
+#endif
+
+    private void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+        contollerHitResetTimeout = 0.1f;
+
+        IsGrounded = Vector3.Angle(hit.normal, Vector3.up) <= 35;
+
+
+        var normalAngle = Quaternion.FromToRotation(hit.normal, Vector3.down);
+
+        var deltaVelocity = normalAngle * velocity;
+        deltaVelocity.y = Mathf.Min(0, deltaVelocity.y);
+
+
+        if (IsGrounded)
+        {
+            deltaVelocity.x = 0;    
+            deltaVelocity.z = 0;
+        }
+
+        // deltaVelocity.x = Mathf.MoveTowards(deltaVelocity.x, 0, hit.collider.material.dynamicFriction / 10);
+        // deltaVelocity.z = Mathf.MoveTowards(deltaVelocity.z, 0, hit.collider.material.dynamicFriction / 10);
+
+        velocity = Quaternion.Inverse(normalAngle) * deltaVelocity;
+    }
+
+    private void OnMove(InputValue inputValue)
+    {
+        var input = inputValue.Get<Vector2>();
+        movementDirection = new Vector3(input.x, 0, input.y); 
+    }
+
+    private void OnLook(InputValue inputValue)
+    {
+        lookDirection += inputValue.Get<Vector2>() * characterSense;
+
+        lookDirection.y = Mathf.Clamp(lookDirection.y, -89, 89);
+    }
+
+    private void OnAttack(InputValue inputValue)
+    {
+        if (inputValue.isPressed)
+        {
+            bool randomPunch = Random.value > 0.5;
+            if (randomPunch)
+                _animator.SetTrigger("isPunchAttackRight");
+            else
+                _animator.SetTrigger("isPunchAttackLeft");
+        }
+    }
+
+    private void OnJump(InputValue inputValue)
+    {
+        if (inputValue.isPressed && IsGrounded)
+        {
+            _animator.SetTrigger("isJump");
+            velocity = Vector3.up * 3;
+        }
+    }
+
+    private void OnSprint(InputValue inputValue)
+    {
+        SprintState = inputValue.isPressed;
+    }
+
+    private Vector3 CalculateMovementDirection()
+    {
+        localMovementAcelerationVector = Vector3.Lerp(localMovementAcelerationVector, transform.rotation * movementDirection * (SprintState ? sprintSpeed : speed), (IsGrounded ? 10 : 1) * Time.fixedDeltaTime);
+
+        return localMovementAcelerationVector; 
+    }
+    private void CalculateVelocity(ref Vector3 velocity)
+    {
+        velocity = Vector3.Lerp(velocity, Physics.gravity, Time.fixedDeltaTime);
+    }
+
+    private void ResetCollisionData()
+    {
+        contollerHitResetTimeout -= Time.fixedDeltaTime;
+        
+        if (contollerHitResetTimeout < 0)
+        {
+            IsGrounded = false;
+        }
     }
 }
